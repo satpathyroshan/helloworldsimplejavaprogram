d438bc98f45e9ff807838ddf12e1c900af5bc6ea
satpathyroshan <roshankumarsatpathy8@gmail.com>
2025-03-23 14:11:53 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch38 team with Praveen, Mustafa");
-      System.out.println("Hello Batch batch38 WITH FOR STUDENTS RUNNING in front of me and Suresh, Prashant in making CI automated vikas teach devops CI proecss");
+     System.out.println("Hello guys my name is Roshan");
+      System.out.println("I am a batch 37 student ");
     }  
 } 
